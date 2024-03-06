5c53670054a924ee7daf7dbc566378365e40303a
Chandler Gipson <chandlergipson@chandlerslaptop.lan>
2024-03-06 13:59:53 -0800

    intial projects

---
+++ REACTJS-INTERVIEW-PROJECTS/src/components/accordian/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id : '1',
+        question: "what are accordian components?",
+        answer: "accodrian componets are user interface elements used for organizing"
+    },
+    {
+        id : '2',
+        question: "what are accordian components?",
+        answer: "accodrian componets are user interface elements used for organizing",
+    },
+    {
+        id : '3',
+        question: "what are accordian components?",
+        answer: "accodrian componets are user interface elements used for organizing",
+    }
+]
+
+
+
+
+export default data
---
+++ REACTJS-INTERVIEW-PROJECTS/src/components/accordian/index.jsx
@@ -0,0 +1,9 @@
+//single selection accordian
+//multiple silection accordian
+
+export default function Accordian() {
+
+    const [selected, setSelected] = useState(null);
+
+return <div>Accordian</div>;
+}
